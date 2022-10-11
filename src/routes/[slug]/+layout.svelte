68145555b7cba8068ff713d98a728e0e03c05247
Adam Hopkins <adam@amhopkins.com>
2022-10-11 13:42:58 +0300

    Cleanup cache

---
+++ src/routes/[slug]/+layout.svelte
@@ -0,0 +1,95 @@
+<script>
+    import "$lib/styles/index.scss"
+    import "prismjs/themes/prism-okaidia.css"
+	import Footer from "$lib/elements/Footer.svelte";
+
+    let isActive = false
+</script>
+
+<nav class="navbar" aria-label="main navigation">
+    <div class="navbar-brand">
+        <a class="navbar-item" href="/">
+            <img
+                src="/images/adam.svg" alt="Adam Hopkins" />
+        </a>
+
+        <a
+            href="/"
+            role="button"
+            class="navbar-burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+            on:click={e => {
+                e.preventDefault()
+                isActive = !isActive
+                return false
+            }
+            }
+        >
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+        </a>
+    </div>
+
+    <div id="article-navbar" class="navbar-menu is-dark" class:is-active={isActive}>
+        <div class="navbar-start">
+            <a class="navbar-item" href="/">
+                <i class="las la-home"></i>
+                Home
+            </a>
+
+            <a class="navbar-item" href="https://sanicbook.com" target="_blank">
+                <i class="las la-book-open"></i>
+                Sanic Book
+            </a>
+
+            <div class="navbar-item has-dropdown is-hoverable">
+                <span class="navbar-link" id="article-menu-title" />
+                <div class="navbar-dropdown" id="article-menu-content" />
+            </div>
+        </div>
+
+        <div class="navbar-end">
+            <a class="navbar-item" href="https://github.com/ahopkins/personal-site">
+                <i class="lab la-github"></i>
+            </a>
+        </div>
+    </div>
+</nav>
+<main>
+    <slot />
+</main>
+
+
+<Footer />
+
+<style scoped>
+    .navbar-menu,
+    .navbar {
+        background-color: #111111;
+    }
+
+    .navbar-item.has-dropdown:focus .navbar-link,
+    .navbar-item.has-dropdown:hover .navbar-link,
+    .navbar-item.has-dropdown.is-active .navbar-link,
+    .navbar-dropdown,
+    .navbar-item.has-dropdown:hover,
+    .navbar-link:hover,
+    a.navbar-item:hover,
+    a.navbar-link.is-active {
+        background-color: #111111 !important;
+        color: #a0a0a0 !important;
+        border-top-color: #111111;
+    }
+
+    .navbar-link::after {
+        border-color: #a0a0a0;
+    }
+    :global(.navbar-dropdown >  .navbar-item) {
+        align-items: flex-start;
+        display: flex;
+        flex-direction: column;
+    }
+</style>
---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,123 @@
+<script>
+    import { onMount } from "svelte"
+    import { readingTime } from "reading-time-estimator"
+    import Time from "svelte-time"
+    import Giscus from "@giscus/svelte"
+    export let data
+
+    const { meta, body } = data
+    let timeToRead = ""
+
+    /**
+     * @param {Node} node
+     */
+    const _extract = (node) => {
+        const output = {}
+        let link = node.firstChild
+        if (link && link.nodeName === "P") {
+            link = link.firstChild
+        }
+        output.text = link?.textContent
+
+        // @ts-ignore
+        output.href = link?.getAttributeNode("href")?.textContent
+        const nested = [...node.childNodes].filter((item) => item.nodeName === "UL" )
+        if (nested.length) {
+            output.children = extractToc(nested[0])
+        }
+        return output
+    }
+
+    /**
+     * @param {Node} toc
+     */
+    const extractToc = (toc) => {
+        return [...toc.childNodes].filter(x => x.nodeName === "LI").map(_extract)
+    }
+
+    const inject = (node, item) => {
+        const outer = document.createElement("div")
+        const newLink = document.createElement('a')
+        outer.className = "navbar-item"
+        newLink.href = item.href
+        newLink.text = item.text
+        outer.appendChild(newLink)
+        if (item.children?.length) {
+            const wrapper = document.createElement("div")
+            wrapper.className = "navbar-indent"
+            item.children.forEach((child) => inject(wrapper,child))
+            outer.appendChild(wrapper)
+        }
+        node.appendChild(outer)
+    }
+
+    onMount(() => {
+        const content = document.querySelector(".content")?.textContent
+        if (content) {
+            timeToRead = readingTime(content).text
+        }
+        const toc = document.querySelector("#table-of-contents+ul")
+        const articleMenuTitle = document.querySelector("#article-menu-title")
+        const articleMenuContent = document.querySelector("#article-menu-content")
+        if (articleMenuTitle) {
+            articleMenuTitle.innerHTML = meta.title
+        }
+        if (articleMenuContent) {
+            articleMenuContent.replaceChildren()
+            if (toc) {
+                const structured =extractToc(toc)
+                structured.forEach(item => {
+                    inject(articleMenuContent, item)
+                })
+            }
+        }
+    })
+</script>
+
+
+<article>
+    <div class="article-container container is-max-desktop">
+        <h1 class="title is-size-1">{meta.title}</h1>
+        <div class="subtitle">{meta.description}</div>
+
+        <div class="article-info">
+            <div class="date">
+                <i class="las la-calendar"></i>
+                <Time timestamp={meta.date} live relative format="YYYY-MM-DD" />
+            </div>
+            <div class="tags">
+                <i class="las la-tags"></i>
+                {#each meta.tag as tag}
+                    <span class="tag is-dark">{tag}</span>
+                {/each}
+            </div>
+            <div class="read-time">
+                <i class="las la-hourglass"></i>
+                {timeToRead}
+            </div>
+        </div>
+        <div class="content">
+            <svelte:component this={body} />
+        </div>
+
+        <Giscus
+            repo="ahopkins/personal-site"
+            repoId="MDEwOlJlcG9zaXRvcnkxMDcxODQzMDY="
+            category="Comments"
+            categoryId="DIC_kwDOBmOAss4CR1uV"
+            mapping="title"
+            strict="0"
+            reactionsEnabled="1"
+            emitMetadata="0"
+            inputPosition="bottom"
+            lang="en"
+            theme="dark"
+        />
+    </div>
+</article>
+
+<svelte:head>
+    <title>
+        {meta.title}
+    </title>
+</svelte:head>
